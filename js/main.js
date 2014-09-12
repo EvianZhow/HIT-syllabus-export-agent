76cb7684725125f5e20a1ddc0b0348cda745986f
Yifei Zhou <zhouyifei@me.com>
2014-09-12 17:43:58 +0800

    finish clop part, ready to submit to chrome store

--- js/main.js
+++ js/main.js
@@ -88,6 +88,68 @@ function classtable_parse(doc, semester_begin_time, csv_content) {
 	download("classtable.csv", csv_content);
 }
 
+function clop_parse(doc, semester_begin_time, csv_content) {
+	var table = doc.querySelector('body > div > div.middle > div.right > div > div > div.table-container > table > tbody');
+	if (table.querySelectorAll('tr') != null) {
+		for (var row = 1; row <= table.querySelectorAll('tr').length; row++) {
+			var cell = table.querySelector('tr:nth-child('+row+')');
+			var c_name = cell.querySelector('td:nth-child(3)').textContent;
+			var c_location = cell.querySelector('td:nth-child(4)').textContent;
+			var c_seat = cell.querySelector('td:nth-child(5)').textContent;
+			var c_week = Number(cell.querySelector('td:nth-child(6)').textContent);
+			var c_weekday = cell.querySelector('td:nth-child(7)').textContent;
+			var c_time = cell.querySelector('td:nth-child(8)').textContent;
+			var start_time = (c_week-1)*604800000+semester_begin_time;
+			var end_time = (c_week-1)*604800000+semester_begin_time;
+			switch (c_weekday) {
+				case "一":
+					start_time += 0;
+					end_time += 0;
+					break;
+				case "二":
+					start_time += 86400000;
+					end_time += 86400000;
+					break;
+				case "三":
+					start_time += 86400000*2;
+					end_time += 86400000*2;
+					break;
+				case "四":
+					start_time += 86400000*3;
+					end_time += 86400000*3;
+					break;
+				case "五":
+					start_time += 86400000*4;
+					end_time += 86400000*4;
+					break;
+				default:
+					break;
+			};
+			switch (c_time) {
+				case "3~4":
+					start_time += 36000000;
+					end_time += 45000000;
+					break;
+				case "5~6":
+					start_time += 46800000;
+					end_time += 55800000;
+					break;
+				case "7~8":
+					start_time += 56400000;
+					end_time += 65400000;
+					break;
+				default:
+					break;
+			};
+			// Generate CSV Code below
+			var start_d = new Date(start_time);
+			var end_d = new Date(end_time);
+			csv_content += c_name+','+start_d.toLocaleString().replace(" ",",")+','+end_d.toLocaleString().replace(" ",",")+','+'False,\"座位号:'+c_seat+'\",\"'+c_location+'\",True\n';
+		};
+	};
+	download("clop.csv", csv_content);
+}
+
 function download(filename, text) {
     var pom = document.createElement('a');
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -100,6 +162,8 @@ function parse() {
 	var d = new Date(2014,8,8);
 	if (document.URL == "http://xscj.hit.edu.cn/hitjwgl/xs/kbcx_bx.asp") {
 		classtable_parse(document, d.getTime(), "Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private\n");
+	} else if (document.URL == "http://clop.hit.edu.cn/listBookingInfos.action") {
+		clop_parse(document, d.getTime(), "Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private\n");
 	}
 }
 
@@ -114,6 +178,11 @@ function insert_btn(node) {
 if (document.URL == "http://xscj.hit.edu.cn/hitjwgl/xs/kbcx_bx.asp") {
 	if (document.querySelector('#spacemain > div.center > table:nth-child(3)') != null) {
 		insert_btn(document.querySelector('#spacemain > div.center > table:nth-child(3)'));
+		document.getElementById('csv_btn').addEventListener("click", parse, false);
+	}
+} else if (document.URL == "http://clop.hit.edu.cn/listBookingInfos.action") {
+	if (document.querySelector('body > div > div.middle > div.right > div') != null) {
+		insert_btn(document.querySelector('body > div > div.middle > div.right > div'));
 		document.getElementById('csv_btn').addEventListener("click", parse, false);
 	}
 }
